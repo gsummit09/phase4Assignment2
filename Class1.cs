957281fac992b18e3a20d8848ad815815400d300
gsummit09 <nayaksumit998@gmail.com>
2024-02-15 15:29:48 +0530

    First Commit

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment2
+{
+    public class Class1
+    {
+        public int checkFact(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * checkFact(n - 1);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppAssignment2;
+Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Enter the Number");
+int value = 5;//Convert.ToInt32(Console.ReadLine());
+int num;
+Class1 fact = new Class1();
+num = fact.checkFact(value);
+Console.WriteLine("Value is:{0}", num);
+Console.ReadLine();
